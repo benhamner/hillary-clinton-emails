7bd16f9f53243b4b2c9b4541b8fe7abcdd357939
Ben Hamner <ben@benhamner.com>
2015-09-10 15:11:48 -0700

    fixed a couple aliases

--- scripts/outputCsvs.py
+++ scripts/outputCsvs.py
@@ -1,6 +1,16 @@
 import numpy as np 
 import pandas as pd 
 
+def normalize_address(raw_address):
+    for c in ["'", ",", "°", "•", "`", '"', "‘", "-"]:
+        raw_address = raw_address.replace(c, "")
+    raw_address = raw_address.lower()
+    if "<" in raw_address:
+        prefix = raw_address[:raw_address.index("<")].strip()
+        if prefix:
+            return prefix
+    return raw_address.strip()
+
 emails = pd.read_csv("input/emailsNoId.csv")
 emails["MetadataTo"].replace(np.nan, "", inplace=True)
 emails["ExtractedTo"].replace(np.nan, "", inplace=True)
@@ -11,6 +21,7 @@ emails.insert(0, "Id", list(range(1, len(emails)+1)))
 emails.insert(5, "SenderPersonId", np.nan)
 
 alias_person = pd.read_csv("versionedInput/alias_person.csv")
+alias_person["AliasName"] = [normalize_address(alias) for alias in alias_person["AliasName"]]
 
 persons = pd.DataFrame(columns=["Id", "Name"])
 aliases = pd.DataFrame(columns=["Id", "Alias", "PersonId"])
@@ -28,16 +39,6 @@ def add_alias(aliases, persons, alias_name, person_name):
     alias_id = len(aliases)+1
     aliases.loc[alias_id-1] = [alias_id, alias_name.lower(), person_id]
 
-def normalize_address(raw_address):
-    for c in ["'", ",", "°", "•", "`", '"', "‘", "-"]:
-        raw_address = raw_address.replace(c, "")
-    raw_address = raw_address.lower()
-    if "<" in raw_address:
-        prefix = raw_address[:raw_address.index("<")].strip()
-        if prefix:
-            return prefix
-    return raw_address.strip()
-
 for (i, alias_person) in alias_person.iterrows():
     add_alias(aliases, persons, alias_person["AliasName"], alias_person["PersonName"])
 
@@ -45,16 +46,18 @@ log = open("working/outputCsvsLog.txt", "w")
 
 for (i, email) in emails.iterrows():
     from_person_id = None
-    if email["MetadataFrom"] != "":
-        locs = np.where(aliases["Alias"]==email["MetadataFrom"].lower())[0]
+    from_address = normalize_address(email["MetadataFrom"].split(";")[0])
+    if from_address != "":
+        locs = np.where(aliases["Alias"]==from_address)[0]
         if len(locs)==0:
-            add_alias(aliases, persons, email["MetadataFrom"], email["MetadataFrom"])
-            log.write("Added From Person: %s\n" % email["MetadataFrom"])
-        loc = np.where(aliases["Alias"]==email["MetadataFrom"].lower())[0][0]
+            add_alias(aliases, persons, from_address, from_address)
+            log.write("Added From Person: %s\n" % from_address)
+        loc = np.where(aliases["Alias"]==from_address)[0][0]
         from_person_id = aliases["PersonId"][loc]
+        from_person_name = persons["Name"][from_person_id-1]
         emails.loc[i, "SenderPersonId"] = from_person_id
         if email["ExtractedFrom"] != "":
-            add_alias(aliases, persons, normalize_address(email["ExtractedFrom"]), email["MetadataFrom"])
+            add_alias(aliases, persons, normalize_address(email["ExtractedFrom"]), from_person_name)
     to_addresses = email["MetadataTo"].split(";") + email["ExtractedTo"].split(";")
     to_addresses = list(set([normalize_address(x) for x in to_addresses]))
     if "" in to_addresses:
